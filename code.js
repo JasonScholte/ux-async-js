6e17d5e58da0c6853518da4458d91f795eed084c
JasonScholte <56888977+JasonScholte@users.noreply.github.com>
2020-09-09 09:09:07 -0500

    finished, thank you for the walkthrough.

--- code.js
+++ code.js
@@ -1,10 +1,17 @@
 window.onload = function () { // Run this once the page has loaded.
     // search
-    document.querySelector("#searchButton").addEventListener("click", searchGithub);
+    document
+    .querySelector("#searchButton")
+    .addEventListener("click", searchGithub);
     function searchGithub() {
         const url = "https://api.github.com/search/users?q="
         const searchUserText = document.querySelector("#searchUser").value;
 
+        fetch(url+searchUserText)
+        .then((response) => response.json())
+        .then((result) => {
+            renderUserList(result.items);
+        });
         // Obtain a list of users from the Github API that match searchUserText
         //  The final result will contain an array under the key 'items'
         // Pass this array to `renderUserList`
@@ -16,6 +23,7 @@ window.onload = function () { // Run this once the page has loaded.
         for (let i = 0; i < githubUsers.length; i++) {
             let githubUser = githubUsers[i];
             html += "<li>";
+            html += `<img style="width: 2rem" src=${githubUser.avatar_url}/>`;
             html += `<strong>${githubUser.login}</strong>`;
             html += `<a target="_blank" href="${githubUser.html_url}">`;
             html += "</li>";
